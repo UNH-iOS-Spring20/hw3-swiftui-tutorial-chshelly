1ada3054d5f539d3831d3ab290b5bd0b3c362242
Shelly <chshelly>
2020-02-27 18:18:57 -0500

    test

---
+++ SupportingView/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by shelly choudhary on 2/27/20.
+//  Copyright © 2020 shelly choudhary. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
